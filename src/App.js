6615af373f83eb6afbca14a4e9edb4e999397bb3
wesenbergg <b.b.j.1998@gmail.com>
2020-09-08 00:55:47 +0300

    init

--- src/App.js
+++ src/App.js
@@ -1,25 +1,35 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from 'react';
+import { Map, Marker, Popup, TileLayer } from 'react-leaflet'
 
-function App() {
+const position = [60.16985569999999, 24.938379]
+
+const App = () => {
+  const [data, setData] = useState([])
+
+  useEffect( () => {
+    fetch("https://api.citybik.es/v2/networks/citybikes-helsinki")
+    .then(res => res.json())
+    .then(res => setData(res.network.stations) )
+  }, [])
+
+  const showMarkers = () => data.map( e => 
+    <Marker position={[e.latitude, e.longitude]}>
+      <Popup>
+        <h3>{e.name}</h3>
+        <p className="my-0">Pyöriä jäljellä: {e.free_bikes}</p>
+        <p className="my-0">Paikkoja vapaana: {e.empty_slots}</p>
+      </Popup>
+    </Marker>
+    )
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Map center={position} zoom={15}>
+    <TileLayer
+      url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+      attribution="&copy; <a href=&quot;http://osm.org/copyright&quot;>OpenStreetMap</a> contributors"
+    />
+    {showMarkers()}
+  </Map>
   );
 }
 
